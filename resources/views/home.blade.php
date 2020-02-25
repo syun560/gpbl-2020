950e25c1a27ac50354ffcbb15df34e6fe69d3600
syun560 <al18056@shibaura-it.ac.jp>
2020-02-25 07:53:22 +0000

    change design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.base')
 
-@section('title', 'duration of service')
+@section('title', 'Duration of service')
 @section('content')
     <p>
-    <li>This graph is about duration of service.</li>
+    <li>This graph is about duration of service at "employee" table.</li>
     </p>
 
-    <table>
-    <tr><th>duration (year)</th><th>count</th></tr>
-    @foreach ($employees_num as $en)
-        <tr>
-        <td>{{$loop->index}}</td>
-        <td>{{$en}}</td>
-        </tr>
-    @endforeach
-    </table>
+    <div class="container">
+        <div class="data">
+            <table>
+            <tr><th>duration</th><th>count</th></tr>
+            @foreach ($employees_num as $en)
+                <tr>
+                <td>{{$loop->index}}</td>
+                <td>{{$en}}</td>
+                </tr>
+            @endforeach
+            </table>
+        </div>
+        <br>
 
-    <br>
-    <canvas id="myChart"></canvas>
+        <div class="graph">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
     <script type="text/javascript" src="https://github.com/nagix/chartjs-plugin-colorschemes/releases/download/v0.2.0/chartjs-plugin-colorschemes.min.js"></script>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,6 +3,7 @@
     <head>
         <title>@yield('title')</title>
         <meta charset="UTF-8">
+        <link href="css/base.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <h1>@yield('title')</h1>
